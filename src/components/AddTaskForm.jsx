053b93a2e93840a4125bdb8227cda4c1ce1fd344
Sabyrka <sabyrka77@gmail.com>
2026-05-06 03:46:54 +0500

    feat: add task creation

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,18 +1,23 @@
 import Field from './Field'
 import Button from './Button'
 
-const AddTaskForm = ({ addTask }) => {
+const AddTaskForm = ({ addTask, newTaskTitle, setNewTaskTitle }) => {
   const onSubmit = (event) => {
     event.preventDefault()
     addTask()
   }
 
   return (
-    <form className="todo__form" onSubmit={onSubmit}>
+    <form
+      className="todo__form"
+      onSubmit={onSubmit}
+    >
       <Field
         className="todo__field"
         label="New task title"
         id="new-task"
+        value={newTaskTitle}
+        onInput={(event) => setNewTaskTitle(event.target.value)}
       />
       <Button type="submit">Add</Button>
     </form>
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,4 +1,4 @@
-const Field = ({ className = '', id, label, type = 'text', onInput }) => {
+const Field = ({ className = '', id, label, type = 'text', value, onInput }) => {
   return (
     <div className={`field ${className}`}>
       <label
@@ -13,6 +13,7 @@ const Field = ({ className = '', id, label, type = 'text', onInput }) => {
         placeholder=" "
         autoComplete="off"
         type={type}
+        value={value}
         onInput={onInput}
       />
     </div>
