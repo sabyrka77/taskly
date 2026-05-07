bccccc3ca16cacb6834b82f8b324d23cbde73b63
Sabyrka <sabyrka77@gmail.com>
2026-05-07 16:17:50 +0500

    refactor: update import paths and rename TaskPage and TasksPage components

--- src/pages/TaskPage/TaskPage.jsx
+++ src/pages/TaskPage/TaskPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import tasksAPI from '../api/tasksAPI'
+import tasksAPI from '../../api/tasksAPI'
 
 const TaskPage = ({ params }) => {
   const taskId = params.id
